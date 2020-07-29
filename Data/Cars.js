6b9a0eba214230e2092cf6e65bc00e458333b9bb
armando44o <armandovelazco2001@gmail.com>
2020-07-29 10:06:10 -0600

    Created database access layer, implemented a read all function

---
+++ Data/Cars.js
@@ -0,0 +1,25 @@
+const client = require('mongodb').MongoClient;
+const assert = require ('assert');
+const { resolve } = require('path');
+//Database object  setup
+const url = process.env.DB_URL;
+const db_name = process.env.DB_NAME; 
+const col_name = process.env.COL_NAME;
+//read all
+const readProducts = () => {
+    const iou = new Promise ((resolve, reject)=>{
+        MongoClient.connection(url, (err, client)=>{
+            assert.equal(err, null);
+
+            const db = client.db(db_name);
+            const collection = db.collection(col_name);
+                collection.find({}).toArray((err, docs) =>{
+                assert.equal(err, null);
+                resolve(docs);
+                client.close();
+                });
+        });
+    });
+    return iou;
+}
+
